62b443c1244dcf84b532df4a1a2180ac4b3e8a14
Giovanni Hislop <giovanni.hislop@dytter.com>
2023-02-03 17:41:35 +0100

    initialize project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+    <section className="chatbox">
+
+    </section>
+    </div>
+  );
+}
+
+export default App;
